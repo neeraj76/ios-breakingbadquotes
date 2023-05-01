cd69d18dc6a1f453c7ed3aef41d5ee731cf8a462
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-05-01 13:16:58 +0530

    Initial Commit

---
+++ BreakingBadQuotes/BreakingBadQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadQuotesApp.swift
+//  BreakingBadQuotes
+//
+//  Created by Neeraj Gupta on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
